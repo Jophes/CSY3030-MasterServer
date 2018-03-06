a43c866e47dd2b4b7bebc41619846ed908b8535c
Joseph Higgins <jopheshiggins@gmail.com>
2018-03-06 19:50:34 +0000

    redid

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const net = require('net');
 
 var serverList = [];
-
+/*
 function Address(_ip, _port) {
     var self = this;
     this.ip = _ip;
     this.port = _port;
+
+    self.exportStr = null;
+    this.export = {
+        get: function() {
+            if (self.exportStr == null) {
+                self.exportStr = JSON.stringify({ip: self.ip, port: self.port});
+            }
+            return self.exportStr;
+        }
+    }
 }
 
 function ServerData(address) {
     var self = this;
 
     this.remote = address;
-}
-
-const { execFile } = require('child_process');
-const child = execFile('node', ['child.js'], (error, stdout, stderr) => {
-  if (error) {
-    throw error;
-  }
-  console.log(stdout);
-});
+}*/
 
 function randomIP() {
     var str = Math.floor(Math.random() * 256).toString();
@@ -31,19 +33,20 @@ function randomIP() {
 }
 
 for (let i = 0; i < 8192; i++) {
-    serverList.push(new ServerData(new Address(randomIP(), 1024 + Math.random() * 8192)));
+    serverList.push({ip: randomIP(), port: Math.floor(1024 + Math.random() * 8192)});
 }
 
 var listener = net.createServer(function(socket) {
-    /*console.log('Connection made');
-    console.log(socket);*/
-    
+    console.log('Connection made');
+
     socket.on('finish', function() {
         console.log('Finish')
     });
     socket.on('end', function() {
         console.log('End');
     });
+    
+    socket.write(JSON.stringify(serverList));
 });
 
 listener.listen(36200);
